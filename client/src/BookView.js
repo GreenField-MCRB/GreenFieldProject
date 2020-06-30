9b683f449af07146ba0d831cc6b845c43c594c20
Moez-Hamza <hamza.moez@gmail.com>
2020-06-30 17:11:17 +0100

    books view working

--- client/src/BookView.js
+++ client/src/BookView.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 
-const Book =({title,author,image,handleClick})=>{
+const Bookview =({title,author,image,handleClick,description,link})=>{
     return (
         <div>
             <h1 className='title'>{title}</h1>
             <h2 className= 'author'>{author}</h2>
             <img src={image} className='bookImage' onClick={handleClick} alt={title}></img>
-    
+            <h3 className='link'>{link}</h3>
+            <h3 className='description'>{description}</h3>
+            <button onClick={handleClick}>go Back</button>
         </div>
     )
     }
-export default Book;
+export default Bookview;
